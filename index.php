0601a7a91d44c78a84a6c0cea78041533935fe24
Gianmarco720 <orabonagianmarco379@gmail.com>
2022-11-28 15:16:48 +0100

    printed hotels on screen

--- index.php
+++ index.php
@@ -39,3 +39,50 @@ $hotels = [
     ],
 
 ];
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Php Hotels</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS v5.2.1 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
+</head>
+
+<body>
+
+    <ul>
+        <?php foreach ($hotels as $hotel) : ?>
+            <li>
+                <h1><?php echo $hotel['name'] ?></h1>
+            </li>
+            <li>
+                Descrizione: <?php echo $hotel['description'] ?>
+            </li>
+            <li>
+                Parcheggio: <?php echo $hotel['parking'] ?>
+            </li>
+            <li>
+                Voto: <?php echo $hotel['vote'] ?>
+            </li>
+            <li>
+                Distanza dal centro: <?php echo $hotel['distance_to_center'] ?>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+
+    <!-- Bootstrap JavaScript Libraries -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+    </script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js" integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
